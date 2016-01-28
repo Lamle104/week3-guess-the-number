c604f0aa884514cefde4fa0ee95848a82bb45960
Lam Le <LamMinhLe@Lams-MacBook-Pro.local>
2016-01-28 10:40:32 -0500

    changed guess the number runner

--- guessTheNumberUpgrade.py
+++ guessTheNumberUpgrade.py
@@ -1,3 +1,9 @@
+import guessTheNumberUpgrade
+
+# this line runs your program
+# using the True parameter should show the random number on the screen
+# using the False parameter hides the random number from the user
+guessTheNumberUpgrade.playGame( False )
 
 # Lam Le
 import random
